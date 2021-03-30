18ddd296c20802410dd43af9cb5a78325270e68c
Hoang Son Tung <megatunger@gmail.com>
2021-03-31 02:20:08 +0700

    Update

--- uCallSystem/main.tf
+++ uCallSystem/main.tf
@@ -1,10 +1,10 @@
 variable "HEROKU_ACCOUNT_EMAIL" {}
-variable "HEROKU_API_KEY" {}
+variable "HEROKU_ACCOUNT_ETRON_TEAM_API_KEY" {}
 variable "CLOUDFLARE_ACCOUNT_EMAIL" {}
 variable "CLOUDFLARE_API_KEY" {}
 variable "CLOUDFLARE_MEGATUNGER_COM_ZONE_ID" {}
 variable "APP_NAME" {
-    default = "ai-academy-landing-page"
+    default = "lua-lep-api"
 }
 
 terraform {
@@ -22,7 +22,7 @@ terraform {
 
 provider "heroku" {
   email = var.HEROKU_ACCOUNT_EMAIL
-  api_key = var.HEROKU_ACCOUNT_MEGATUNGER_API_KEY
+  api_key = var.HEROKU_API_KEY
 }
 
 provider "cloudflare" {
@@ -30,25 +30,26 @@ provider "cloudflare" {
     api_key = var.CLOUDFLARE_API_KEY
 }
 
-resource "heroku_app" "ai_academy" {
+resource "heroku_app" "lualep" {
     name = var.APP_NAME
     region = "eu"
 }
 
-resource "heroku_domain" "ai_academy" {
-  app = heroku_app.ai_academy.id
-  hostname = "aialandingpage.megatunger.com"
+resource "heroku_domain" "lualep" {
+  app = heroku_app.lualep.id
+  hostname = "lualepapi.megatunger.com"
 }
 
-resource "heroku_build" "ai_academy" {
-    app = heroku_app.ai_academy.id
+resource "heroku_build" "lualep" {
+    app = heroku_app.lualep.id
+    buildpacks = ["https://github.com/heroku/heroku-buildpack-python.git", "https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git"]
     source = {
-        url = "https://github.com/megatunger/LandingPageForAIA/archive/master.tar.gz"
+        url = "https://github.com/megatunger/Lua-Lep-Backend/archive/master.tar.gz"
     }
 }
 
-resource "cloudflare_record" "ai_academy" {
-    name = "aialandingpage"
+resource "cloudflare_record" "lualep" {
+    name = "lualepapi"
     value = "${var.APP_NAME}.herokuapp.com"
     zone_id = var.CLOUDFLARE_MEGATUNGER_COM_ZONE_ID
     type = "CNAME"
