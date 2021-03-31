4fd3d2e52b9abbeb976e4bfa5b8052d5585dcd04
Hoang Son Tung <megatunger@gmail.com>
2021-03-31 15:42:07 +0700

    uCallSystem

---
+++ uCallSystemStag/main.tf
@@ -0,0 +1,57 @@
+variable "HEROKU_ACCOUNT_EMAIL" {}
+variable "HEROKU_API_KEY" {}
+variable "CLOUDFLARE_ACCOUNT_EMAIL" {}
+variable "CLOUDFLARE_API_KEY" {}
+variable "CLOUDFLARE_ZONE_ID" {}
+variable "APP_NAME" {
+    default = "ucall-system-stag"
+}
+
+terraform {
+  required_providers {
+    heroku = {
+      source = "heroku/heroku"
+      version = "3.0.0"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.13.2"
+    }
+  }
+}
+
+provider "heroku" {
+  email = var.HEROKU_ACCOUNT_EMAIL
+  api_key = var.HEROKU_API_KEY
+}
+
+provider "cloudflare" {
+    email = var.CLOUDFLARE_ACCOUNT_EMAIL
+    api_key = var.CLOUDFLARE_API_KEY
+}
+
+resource "heroku_app" "ucall_system_stag" {
+    name = var.APP_NAME
+    region = "us"
+}
+
+resource "heroku_domain" "ucall_system_stag" {
+  app = heroku_app.ucall_system_stag.id
+  hostname = "api.stag.ucall.cc"
+}
+
+resource "heroku_build" "ucall_system_stag" {
+    app = heroku_app.ucall_system_stag.id
+    buildpacks = ["https://github.com/heroku/heroku-buildpack-python.git"]
+    source = {
+        url = "https://api.github.com/repos/megatunger/ucall_manager/tarball?access_token=${var.GITHUB_API_TOKEN}"
+    }
+}
+
+resource "cloudflare_record" "ucall_system_stag" {
+    name = "api"
+    value = "${var.APP_NAME}.herokuapp.com"
+    zone_id = var.CLOUDFLARE_ZONE_ID
+    type = "CNAME"
+    proxied = true
+}
