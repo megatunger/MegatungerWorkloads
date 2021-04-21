8b258b453ae6b25c3d4309ceeebd71d2612b7511
Hoang Son Tung <megatunger@gmail.com>
2021-04-22 02:26:31 +0700

    uCall new domain

--- uCallApp/main.tf
+++ uCallApp/main.tf
@@ -6,17 +6,17 @@ variable "CLOUDFLARE_API_KEY" {}
 variable "GITHUB_API_TOKEN" {}
 
 variable "APP_NAME_APP" {
-    default = "ucall-app"
+  default = "ucall-app"
 }
 
 terraform {
   required_providers {
     heroku = {
-      source = "heroku/heroku"
+      source  = "heroku/heroku"
       version = "3.0.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "2.13.2"
     }
   }
@@ -24,36 +24,36 @@ terraform {
 
 
 provider "heroku" {
-  email = var.HEROKU_ACCOUNT_EMAIL
+  email   = var.HEROKU_ACCOUNT_EMAIL
   api_key = var.HEROKU_API_KEY
 }
 
 provider "cloudflare" {
-    email = var.CLOUDFLARE_ACCOUNT_EMAIL
-    api_key = var.CLOUDFLARE_API_KEY
+  email   = var.CLOUDFLARE_ACCOUNT_EMAIL
+  api_key = var.CLOUDFLARE_API_KEY
 }
 
 resource "heroku_app" "ucall_app" {
-    name = var.APP_NAME_APP
-    region = "eu"
+  name   = var.APP_NAME_APP
+  region = "eu"
 }
 
 resource "heroku_domain" "ucall_app" {
-  app = heroku_app.ucall_app.id
-  hostname = "app.ucall.cc"
+  app      = heroku_app.ucall_app.id
+  hostname = "app.ucall.asia"
 }
 
 resource "heroku_build" "ucall_app" {
-    app = heroku_app.ucall_app.id
-    source = {
-        url = "https://api.github.com/repos/megatunger/uCall-App/tarball?access_token=${var.GITHUB_API_TOKEN}"
-    }
+  app = heroku_app.ucall_app.id
+  source = {
+    url = "https://api.github.com/repos/megatunger/uCall-App/tarball?access_token=${var.GITHUB_API_TOKEN}"
+  }
 }
 
 resource "cloudflare_record" "ucall_app" {
-    name = "app"
-    value = "${var.APP_NAME_APP}.herokuapp.com"
-    zone_id = var.CLOUDFLARE_ZONE_ID
-    type = "CNAME"
-    proxied = true
+  name    = "app"
+  value   = "${var.APP_NAME_APP}.herokuapp.com"
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  type    = "CNAME"
+  proxied = true
 }
--- uCallSystemProd/main.tf
+++ uCallSystemProd/main.tf
@@ -5,40 +5,40 @@ variable "CLOUDFLARE_API_KEY" {}
 variable "CLOUDFLARE_ZONE_ID" {}
 variable "GITHUB_API_TOKEN" {}
 variable "APP_NAME" {
-    default = "ucall-system-prod"
+  default = "ucall-system-prod"
 }
 
 terraform {
   required_providers {
     heroku = {
-      source = "heroku/heroku"
+      source  = "heroku/heroku"
       version = "3.0.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "2.13.2"
     }
   }
 }
 
 provider "heroku" {
-  email = var.HEROKU_ACCOUNT_EMAIL
+  email   = var.HEROKU_ACCOUNT_EMAIL
   api_key = var.HEROKU_API_KEY
 }
 
 provider "cloudflare" {
-    email = var.CLOUDFLARE_ACCOUNT_EMAIL
-    api_key = var.CLOUDFLARE_API_KEY
+  email   = var.CLOUDFLARE_ACCOUNT_EMAIL
+  api_key = var.CLOUDFLARE_API_KEY
 }
 
 resource "heroku_app" "ucall_system_prod" {
-    name = var.APP_NAME
-    region = "eu"
+  name   = var.APP_NAME
+  region = "eu"
 }
 
 resource "heroku_domain" "ucall_system_prod" {
-  app = heroku_app.ucall_system_prod.id
-  hostname = "api.ucall.cc"
+  app      = heroku_app.ucall_system_prod.id
+  hostname = "api.ucall.asia"
 }
 
 resource "heroku_addon" "redis" {
@@ -48,17 +48,17 @@ resource "heroku_addon" "redis" {
 
 
 resource "heroku_build" "ucall_system_prod" {
-    app = heroku_app.ucall_system_prod.id
-    buildpacks = ["https://github.com/heroku/heroku-buildpack-python.git"]
-    source = {
-        url = "https://api.github.com/repos/megatunger/ucall_manager/tarball?access_token=${var.GITHUB_API_TOKEN}"
-    }
+  app        = heroku_app.ucall_system_prod.id
+  buildpacks = ["https://github.com/heroku/heroku-buildpack-python.git"]
+  source = {
+    url = "https://api.github.com/repos/megatunger/ucall_manager/tarball?access_token=${var.GITHUB_API_TOKEN}"
+  }
 }
 
 resource "cloudflare_record" "ucall_system_prod" {
-    name = "api"
-    value = "${var.APP_NAME}.herokuapp.com"
-    zone_id = var.CLOUDFLARE_ZONE_ID
-    type = "CNAME"
-    proxied = true
+  name    = "api"
+  value   = "${var.APP_NAME}.herokuapp.com"
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  type    = "CNAME"
+  proxied = true
 }
